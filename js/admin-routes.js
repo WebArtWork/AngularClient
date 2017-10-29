b9dc08c2127a1a2a283bf042f4f5eae91c8aa98b
Honchar Denys <crackeraki@gmail.com>
2017-10-29 12:34:21 +0200

    added part of design structure

--- js/admin-routes.js
+++ js/admin-routes.js
@@ -1,21 +1,67 @@
-var directives = {};
-app.directive(directives);
-directives.topbar=function(){
+var a_directives = {};
+app.directive(a_directives);
+a_directives.topbar=function(){
 	"ngInject";
 	return {
 		restrict: 'E',
 		templateUrl: '/html/admin/_topbar.html'
 	}
 };
+var ctrl = function($scope, User, $http){
+	var u = $scope.u = User;
+	if(!u.allUsers){
+		$http.get('/api/user/admin/users').then(function(resp){
+			u.allUsers = resp.data;
+		});
+	}
+	u.create = function(email, password) {
+		$http.post('/api/user/admin/create', {
+			password: user.password,
+			email: user.email
+		}).then(function(resp){
+			if(resp.data) u.allUsers.push(resp.data);
+		});
+	}
+	u.update = function(user) {
+		$timeout.cancel(user.ut);
+		$http.post('/api/user/admin/update', {
+			avatarUrl: user.avatarUrl,
+			skills: user.skills,
+			followings: user.followings,
+			followers: user.followers,
+			gender: user.gender,
+			name: user.name,
+			birth: user.birth,
+			data: user.data,
+			_id: user._id
+		});
+	}
+	u.updateAfterWhile = function(user){
+		$timeout.cancel(user.ut);
+		user.ut = $timeout(function(){
+			u.update(user);
+		}, 1000);
+	}
+	u.delete = function(user){
+		$http.post('/api/user/admin/delete', {
+			_id: user._id
+		});
+	}
+	u.changePassword = function(newPass){
+		$http.post('/api/user/admin/changePassword',{
+			newPass: newPass
+		});
+	}
+}
 app.config(function($stateProvider, $locationProvider, $urlRouterProvider) {
 	var root = '/Admin';
 	$urlRouterProvider.otherwise(root);
 	$stateProvider.state({
 		name: 'Users',
-		url: root,
+		url: root, controller: ctrl,
 		templateUrl: '/html/admin/Users.html'
 	}).state({
-		name: 'Profile',
+		name: 'Profile', controller: ctrl,
 		url: root+'/Profile/:_id',
 		templateUrl: '/html/admin/Profile.html'
 	});
--- js/crud.js
+++ js/crud.js
@@ -1,88 +1,159 @@
-var services = {};
-app.service(services);
-var filters = {};
-app.filter(filters);
-var directives = {};
-app.directive(directives);
+var services = {}, filters = {}, directives = {}, controllers = {};
+app.service(services).filter(filters).directive(directives).controller(controllers);
 services.socket = function(){
+	"ngInject";
 	var loc = window.location.host;
 	var socket = io.connect(loc);
 	socket.on('disconnect', function(){
 		socket = io.connect(loc);
 	});
+	this.emit = function(){
+
+	}
+	this.on = function(){
+
+	}
 }
+services.img=function(){
+	"ngInject";
+	this.resizeUpTo = function(info, callback){
+		if(!info.file) return console.log('No image');
+		info.width = info.width || 1920;
+		info.height = info.height || 1080;
+		if(info.file.type!="image/jpeg" && info.file.type!="image/png")
+			return console.log("You must upload file only JPEG or PNG format.");
+		var reader = new FileReader();
+		reader.onload = function (loadEvent) {
+			var ratioToFloat = function(val) {
+				var r = val.toString(),
+					xIndex = r.search(/[x:]/i);
+				if (xIndex > -1) {
+					r = parseFloat(r.substring(0, xIndex)) / parseFloat(r.substring(xIndex + 1));
+				} else {
+					r = parseFloat(r);
+				}
+				return r;
+			};
+			var canvasElement = document.createElement('canvas');
+			var imageElement = document.createElement('img');
+			imageElement.onload = function() {
+				var ratioFloat = ratioToFloat(info.width/info.height);
+				var imgRatio = imageElement.width / imageElement.height;
+				if (imgRatio < ratioFloat) {
+					width = info.width;
+					height = width / imgRatio;
+				} else {
+					height = info.height;
+					width = height * imgRatio;
+				}
+				canvasElement.width = width;
+				canvasElement.height = height;
+				var context = canvasElement.getContext('2d');
+				context.drawImage(imageElement, 0, 0 , width, height);
+				callback(canvasElement.toDataURL('image/png', 1));
+			};
+			imageElement.src = loadEvent.target.result;
+		};
+		reader.readAsDataURL(info.file);
+	}
+};
+directives.fm = function(){
+	"ngInject";
+	return{
+		restrict: 'E',
+		controller: function(img, User, $timeout){
+			angular.element(document.getElementById("profileID"))
+			.bind('change', function(evt) {
+				img.resizeUpTo({
+					file: evt.currentTarget.files[0],
+					width: 300,
+					height: 300
+				}, function(dataUrl) {
+					$timeout(function() {
+						User.avatarUrl = dataUrl;
+						User.save();
+					});
+				});
+			});
+		},
+		template: '<input type="file" ng-hide="true" id="profileID">'
+	}
+}
+/*
+*	Controllers
+*/
+controllers.Users = function($scope, User){
+	"ngInject";
+	console.log('CALLEd');
+	$scope.User = User;
+}
+
 /*start_user*/
 /*
 *	Crud file for client side user
+*	We don't use waw crud on the user as it's basically personal update.
+*	And if user use more then one device we can easly handle that with sockets.
 */
-crudServices.User = function($http, $timeout, socket){
+services.User = function($http, $timeout){
 	// Initialize
-		var srv = {};
+		var self = this, ut;
+		this.all_skills = ['cooking','fishing','painting'];
+		$http.get('/api/user/get').then(function(resp){
+			self.avatarUrl = resp.data.avatarUrl;
+			self.skills = resp.data.skills;
+			self.gender = resp.data.gender;
+			self.name = resp.data.name;
+			self.birth = resp.data.birth;
+			self.data = resp.data.data;
+			self._id = resp.data._id;
+			$http.get('/api/user/users').then(function(resp){
+				self.users = resp.data.users;
+			});
+		});
+	// Skills
+		this.addSkill = function(skill){
+
+			self.update();
+		}
+		this.removeSkill = function(skill){
+
+			self.update();
+		}
+	// Follow
+		this.follow = function(user){
+
+		}
+		this.unfollow = function(user){
+
+		}
 	// Routes
-		srv.update = function(obj, callback){
-			if(!obj) return;
-			$timeout.cancel(obj.updateTimeout);
-			$http.post('/api/user/update', obj)
-			.then(function(){
-				if(typeof callback == 'function')
-					callback();
+		this.update = function(){
+			$timeout.cancel(ut);
+			$http.post('/api/user/update', {
+				avatarUrl: self.avatarUrl,
+				skills: self.skills,
+				gender: self.gender,
+				name: self.name,
+				birth: self.birth,
+				date: self.date,
+				_id: self._id
 			});
 		}
-		srv.updateAfterWhile = function(obj){
-			$timeout.cancel(obj.updateTimeout);
-			obj.updateTimeout = $timeout(function(){
-				srv.update(obj);
-			}, 1000);
+		this.updateAfterWhile = function(){
+			$timeout.cancel(ut);
+			ut = $timeout(self.update, 1000);
 		}
-		srv.delete = function(obj, callback){
-			if(!obj) return;
-			$http.post('/api/user/delete', obj)
-			.then(function(){
-				if(typeof callback == 'function')
-					callback();
-				socket.emit('MineUserDeleted', obj);
-			});
+		this.delete = function(){
+			$http.get('/api/user/delete');
 		}
-		srv.logout = function(callback){
-			if(!obj) return;
-			$http.post('/api/user/logout')
-			.then(function(){
-				if(typeof callback == 'function')
-					callback();
-			});
-		}
-		srv.changePassword = function(oldPass, newPass){
+		this.changePassword = function(oldPass, newPass){
 			if(!oldPass||!newPass) return;
 			$http.post('/api/user/changePassword',{
 				oldPass: oldPass,
 				newPass: newPass
-			}).then(function(resp){
-				if(resp.data){
-					socket.emit('MineUserUpdated', {
-						logout: true
-					});
-				}
-			});
-		}
-		srv.changeAvatar = function(user, dataUrl){
-			$timeout(function(){
-				user.avatarUrl = dataUrl;
-			});
-			$http.post('/api/user/changeAvatar', {
-				dataUrl: dataUrl
-			}).then(function(resp){
-				if(resp.data){							
-					$timeout(function(){
-						user.avatarUrl = resp.data;
-						socket.emit('MineUserUpdated', {
-							avatarUrl: resp.data
-						});
-					});
-				}
 			});
 		}
 	// End of service
-	return srv;
 }
 /*
 *	End for User Crud.
