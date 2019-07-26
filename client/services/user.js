e3de9abb8652bbe75f82a7c5f61389934ce9e261
Honchar Denys <crackeraki@gmail.com>
2019-07-26 11:07:47 +0300

    update to work with waw 1.x

--- client/services/user.js
+++ client/services/user.js
@@ -1,6 +1,25 @@
-/*
-*	will handle all data which touch user part.
-*/
-app.service('user', [function(){
-	
+app.service('user', ['$http', 'mongo', function($http, mongo){
+	var self = this;
+	$http.get('/api/user/me').then(function(resp){
+		for (var key in resp.data) {
+			self[key] = resp.data[key];
+		}
+		self.users = mongo.get('user');
+	});
+	this.update = function(){
+		mongo.afterWhile(self, ()=>{
+			mongo.updateAll('user', self, {
+				fields: '_id name'
+			});
+		});
+	}
+	this.change_password = function(oldPass, newPass){
+		$http.post('/api/user/changePassword', {
+			newPass: newPass,
+			oldPass: oldPass
+		}, function(resp){
+			if(resp) alert('successfully changed password');
+			else alert('failed to change password');
+		});	
+	}
 }]);
--- server/user/client/server.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app, sd) {
-	sd._page('/Login', 'Login', {
-		page: 'Land'
-	});
-};
